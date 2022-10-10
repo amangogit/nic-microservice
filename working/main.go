c1d07b2e1848570c83626c63c91b114c8ab19f5d
amangogit <aman.devacc@gmail.com>
2022-10-10 23:51:04 +0530

    added simple microservice based on lecture 1

---
+++ working/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request){
+		log.Println("Hello World!")
+		d, err := ioutil.ReadAll(req.Body)
+		if err != nil{
+			http.Error(resp, "Oops", http.StatusBadRequest)
+			// resp.WriteHeader(http.StatusBadRequest)
+			// resp.Write([]byte("Oops"))
+			return
+		}
+
+		log.Printf("Data %s\n",d)
+
+		fmt.Fprintf(resp, "Hello %s",d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
+		log.Println("Goodbye World!")
+	})
+
+	// bind address
+	http.ListenAndServe(":9990", nil)
+}
